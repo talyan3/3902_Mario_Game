e4f1f2d7760db3749ebe91d73d45575e8ea442ad
DivineInputs <159392987+DivineInputs@users.noreply.github.com>
2025-11-17 23:32:01 -0500

    basic pipe logic been added

--- MonogameTest/obj/Debug/net8.0/MonogameTest.AssemblyInfo.cs
+++ MonogameTest/obj/Debug/net8.0/MonogameTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8623786caccf3a4278dd8b646ca3bd8c097c5941")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eea6a3d8c8928cc6cb5afef593d74c2882230c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
