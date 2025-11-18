2427e22cb7cfc29b9504f2fda8f652b5d9038209
Jahnavi-Acharya <58119500+Jahnavi-Acharya@users.noreply.github.com>
2025-11-17 21:33:48 -0500

    Update PowerupCoinState.cs

--- MonogameTest/Collision/PowerupCoinState.cs
+++ MonogameTest/Collision/PowerupCoinState.cs
@@ -1,15 +1,32 @@
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics; 
+
 namespace MonogameTest;
 
 public class PowerupCoinState : PowerupState
 {
-    public PowerupCoinState(PowerupInstance p) : base(p) {}
+    private float _riseSpeed = 40f;   
+    private float _lifeTime = 0.4f;   
+    private float _elapsed = 0f;
 
-    public override void Enter() {}
+    public PowerupCoinState(PowerupInstance p) : base(p) { }
+
+    public override void Enter()
+    {
+        _elapsed = 0f;
+    }
 
     public override void Update(GameTime gameTime)
     {
-        // coin does nothing
+        float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        _elapsed += dt;
+
+       
+        _powerup.Position.Y -= _riseSpeed * dt;
+
+        
+        if (_elapsed >= _lifeTime)
+        {
+            _powerup.IsAlive = false;
+        }
     }
 }
