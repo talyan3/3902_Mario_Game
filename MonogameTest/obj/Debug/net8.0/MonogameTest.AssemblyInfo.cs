4f0bfbcee4a21b6f0d6f5a06b3502541932374cd
Anika Talyan <anikatalyan@gmail.com>
2025-12-08 12:10:41 -0500

    changed files a little bit

--- MonogameTest/obj/Debug/net8.0/MonogameTest.AssemblyInfo.cs
+++ MonogameTest/obj/Debug/net8.0/MonogameTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b84682965f117eb34604b9ab0bc92eee2d3ecb98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61b07b087e1f2cbfd9ca1bc2fae9bac875dbf2a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
