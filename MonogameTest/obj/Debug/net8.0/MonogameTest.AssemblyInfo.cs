61b07b087e1f2cbfd9ca1bc2fae9bac875dbf2a6
Anika Talyan <anikatalyan@gmail.com>
2025-12-08 10:45:39 -0500

    added sprites

--- MonogameTest/obj/Debug/net8.0/MonogameTest.AssemblyInfo.cs
+++ MonogameTest/obj/Debug/net8.0/MonogameTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+353cceb90d4475e330aa887ca4512ea178e7a905")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b84682965f117eb34604b9ab0bc92eee2d3ecb98")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
