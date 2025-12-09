9e9236d7e37b0d9ce2a1781e9380e708f20e73d2
Anika Talyan <anikatalyan@gmail.com>
2025-12-08 23:44:29 -0500

    just ipdating

--- MonogameTest/obj/Debug/net8.0/MonogameTest.AssemblyInfo.cs
+++ MonogameTest/obj/Debug/net8.0/MonogameTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5a64b71638a19f5dfdc5ac3004b91bb925bc0e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0555632eab6a1aa6a9f30253e4342d2b973070f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
