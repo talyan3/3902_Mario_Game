1b58a45af0421a368f1708b87fd56b56ba4b5c12
Anika Talyan <anikatalyan@gmail.com>
2025-12-10 15:18:48 -0500

    added christmas sprites

--- MonogameTest/obj/Debug/net8.0/MonogameTest.AssemblyInfo.cs
+++ MonogameTest/obj/Debug/net8.0/MonogameTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86fe0de258d5a06e232b6ef7d38bc40a879cf61e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cb4e179f19058baef331ccf9b7b8e2f8941edd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
