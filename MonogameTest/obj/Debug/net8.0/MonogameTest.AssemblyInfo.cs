9f01083c030128c34969946e97c422571113da64
Anika Talyan <anikatalyan@gmail.com>
2025-12-10 17:50:10 -0500

    added more tam sprites

--- MonogameTest/obj/Debug/net8.0/MonogameTest.AssemblyInfo.cs
+++ MonogameTest/obj/Debug/net8.0/MonogameTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d9d82228aba6f120ebcbc52ea2552fa9fc6e70c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f56fbb266836ebd0dddd422fcdd44ee1c8a221c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonogameTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
